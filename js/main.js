df5d47b4d9a5442cd86c8372fc3fb2356fa6f60e
qwer999 <qwer999@gmail.com>
2017-03-17 11:22:42 +0900

    디자인 반영

---
+++ js/main.js
@@ -0,0 +1,1 @@
+window.console&&console.log("%c \ud83d\ude0b contact - qwer999@gmail.com ","color:white; background:#F5127F; padding:5px; display:block; border-radius:100px;"),$(window).ready(function(){$(".shadow-color").each(function(){var o=$(this).clone().addClass("shadow");$(this).after(o)})});
